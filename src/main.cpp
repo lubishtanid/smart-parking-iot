f971d80759a29ee9b3faa02e1c6799f59bfef03f
Dardan Lubishtani <dardlubishtani@gmail.com>
2023-09-11 19:44:48 +0200

    OLED display u8g2lib definitions

--- src/main.cpp
+++ src/main.cpp
@@ -34,10 +34,20 @@ const int irSensor4Pin = 33;
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
+#define CUSTOM_SDA_PIN 14
+#define CUSTOM_SCL_PIN 15
+
+TwoWire myWire(0); //0 indicates the first I2C bus
+
+// Create an instance of the U8g2 SH1106 display library with your custom Wire instance
+U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ CUSTOM_SCL_PIN, /* data=*/ CUSTOM_SDA_PIN, /* reset=*/ U8X8_PIN_NONE);
 
 
 void setup() {
   Serial.begin(115200);
+  // Initialize my custom Wire instance
+  myWire.begin(CUSTOM_SDA_PIN, CUSTOM_SCL_PIN);
+
 }
 
 void loop() {
